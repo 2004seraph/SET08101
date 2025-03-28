946b0c9286459cf733e4e30c166eb0f2890e5e50
Sam Taseff <sam.taseff@proton.me>
2025-03-28 13:00:32 +0000

    docs

---
+++ src/js/progress.js
@@ -0,0 +1,1 @@
+// For unlocking later rooms
--- src/js/quests.js
+++ src/js/quests.js
@@ -1,11 +1,11 @@
 "use strict";
 
-// will need to be a module
-
 /**
  * HOW TO USE:
  *
- * 1. Define a quest by setting the data-quest attribute to a unique id, on some text element
+ * 0. Include this file somewhere in your HTML
+ *
+ * 1. Define a quest by setting the data-quest attribute to a unique id, on some text element, like a <p>
  *
  * 2. Use your own JavaScript logic to detect when that quest has been completed
  *
@@ -19,6 +19,20 @@
  * A data-quest attribute can also be followed by a data-on-complete attribute, here you can
  * put some JavaScript that will run when the user completes the quest, it will also run again
  * if the page is reloading and it was completed in the past (requires saveState.js to be loaded)
+ *
+ * EXAMPLE:
+ *
+ * <!-- It's intended you put the attributes on the actual quest titles themselves -->
+ * <h3 data-quest="throwAway">Throw it away</h3>
+ * <h3
+ *    data-quest="pickup"
+ *    data-on-complete="console.log('i have all the stuff!')">Pick up everything</h3>
+ *
+ * <script>
+ *    if (window.quests.isComplete("pickup")) {
+ *      window.quests.complete("pickup");
+ *    }
+ * </script>
  */
 
 (function() {
